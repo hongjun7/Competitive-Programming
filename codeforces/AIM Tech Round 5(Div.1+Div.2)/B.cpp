6c39fc29446c8f8e40929ad18b54725b22e7c1cd
Hongjun Jang <hongjun-7@daum.net>
2018-08-30 12:33:42 +0900

    add #AIM Tech Round 5 #A, #B, #C
    add #A #B.
    need to solve #C again with another solution.

---
+++ codeforces/AIM Tech Round 5(Div.1+Div.2)/B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+
+int main() {
+	scanf("%d%d",&N,&M);
+	for (int i = 1; i <= 1130; i++) printf("5");
+	printf("1");
+	puts("");
+	for (int i = 1; i <= 1130; i++) printf("4");
+	printf("9");
+}
